e4aaefe94acf069359dd75dd7abd912d3b01c0a8
kabigithub <kabilasm211@gmail.com>
2016-10-21 12:26:48 +0530

    Add files via upload

---
+++ pgm1.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int i;
+
+printf("the number is ");
+scanf("%d",$i);
+if(i<0)
+{
+printf("the number is positive");
+}
+if(i>0)
+{
+printf("the number is negative");
+}
+if(i=0)
+{
+printf("the number is zero");
+}
+getch();
+}
