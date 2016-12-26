5461b33f9f05e39c542c7b5858bc84774960c11e
kabigithub <kabilasm211@gmail.com>
2016-12-26 19:01:10 +0530

    Add files via upload

---
+++ ascii.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+int i,j;
+for(i=65;i<=90;i++)
+{
+printf("\n ascii values A to Z %d=%c",i,i);
+}
+for(i=97;i<=122;i++)
+{
+
+    printf("\n ascii values a to z %d=%c",i,i);
+}
+getch();
+}
