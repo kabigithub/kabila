cd25830a069a4f465e15529eb9859f1e2e3e8114
kabigithub <kabilasm211@gmail.com>
2017-06-07 11:45:26 +0530

    Add files via upload

---
+++ character.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+char a[50];
+int count=0,i;
+printf("enter the word");
+scanf("%s",&a[i]);
+
+while(a[i]!=NULL)
+{
+count++;
+i++;
+}
+printf("%d",count);
+return 0;
+getch();
+}
