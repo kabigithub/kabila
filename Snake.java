165a54260aa01fe095b137da66ce3fbf7bff2358
kabigithub <kabilasm211@gmail.com>
2017-07-11 10:49:10 +0530

    Add files via upload

---
+++ Snake.java
@@ -0,0 +1,28 @@
+package test;
+import java.util.Scanner;
+public class SnakeGame {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt();
+		for(int i=0;i<n;i++)
+		{
+			for(int j=0;j<n;j++)
+			{
+				if(i==0||i==(n-1)||j==0||j==(n-1))
+				{
+					System.out.print("*");
+				}
+				else
+				{
+					System.out.print(" ");
+				}
+			}
+				System.out.println(" ");
+			
+		}
+
+	}
+
+}
